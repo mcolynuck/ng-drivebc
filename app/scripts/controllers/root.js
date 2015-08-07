3f3f8af3fd3d085bde76d5409169e9585b2eb3dc
Michael Colynuck <mcolynuck@yahoo.com>
2015-08-07 02:19:19 -0700

    Added sass files and directories

---
+++ app/scripts/controllers/root.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drivebcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the drivebcApp
+ */
+angular.module('drivebcApp')
+  .controller('RootCtrl', ['$scope', '$location', function ($scope, $location) {
+  	$scope.isMapPage = function(){
+  		// True if map page is active
+console.log("isMapPage");
+return true;
+  	};
+  }]);
