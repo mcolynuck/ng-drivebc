720460d497d48b48c207629d5d7ae02fbe8d116d
Michael Colynuck <mcolynuck@yahoo.com>
2015-08-12 19:26:27 -0700

    Added icon images, webcam page works, subscription icons work, DBC Banner added

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drivebcApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the drivebcApp
+ */
+angular.module('drivebcApp')
+  .controller('HomeCtrl', function () {
+  });
